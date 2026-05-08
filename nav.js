cf595447a0db0fe1121a2dd44d036f3e13466583
Wokafishh <arvid.junehag@gmail.com>
2026-05-08 13:20:05 +0200

    Ny blaj

--- nav.js
+++
@@ -1,9 +0,0 @@
-fetch('/nav.html')
-  .then(r => r.text())
-  .then(html => {
-    document.getElementById('nav-placeholder').innerHTML = html;
-    const page = '/' + location.pathname.split('/').filter(Boolean).join('/') + '/';
-    document.querySelectorAll('#nav-placeholder .nav-links a').forEach(a => {
-      if (a.getAttribute('href') === page) a.classList.add('active');
-    });
-  });
